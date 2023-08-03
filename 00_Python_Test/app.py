a0029c78e5f8f7eec74b9aaf8aa71ba5af7f9542
Bade om <71856738+ombade@users.noreply.github.com>
2023-08-03 21:16:58 +0530

    The answer added upto 34

---
+++ 00_Python_Test/app.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+# import json to load json data to python dictionary
+import json
+# urllib.request to make a request to api
+import urllib.request
+
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        ''' api key might be expired use your own api_key
+			place api_key in place of appid ="your_api_key_here " '''
+
+        # source contain JSON data from API
+
+        source = urllib.request.urlopen(
+            'http://api.openweathermap.org/data/2.5/weather?q ='
+            + city + '&appid = your_api_key_here').read()
+
+        # converting JSON data to a dictionary
+        list_of_data = json.loads(source)
+
+        # data for variable list_of_data
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ' '
+            + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + 'k',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+        }
+        print(data)
+    else:
+        data = {}
+    return render(request, "main/index.html", data)
