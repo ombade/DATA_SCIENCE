cff1a14250b6319da8f3f88587d7ce86b4e30b74
Bade om <71856738+ombade@users.noreply.github.com>
2023-08-12 23:01:07 +0530

    web scrapping added

--- Database/MYSQL/create_database.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="abc",
-  password="password"
-)
-mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE if not exists test2")
-mydb.close()
